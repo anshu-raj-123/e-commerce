e94b69de39be6f37c50b887f1a744dc16b7a7f66
Anshu <81515597+anshu-raj-123@users.noreply.github.com>
2023-03-03 22:42:49 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,1 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [require("daisyui")],
 
-  
-  daisyui: {
-    themes: false,
-  },
-}
